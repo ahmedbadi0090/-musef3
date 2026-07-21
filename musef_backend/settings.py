9b80c3ef58f79702266b81962919c0f14af1cf9f
Ahmad <ahmedmbadi0090@gmail.com>
2026-07-21 17:56:14 +0200

    Update BASE_URL with ngrok link

--- musef_backend/settings.py
+++ musef_backend/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-lo34f#*t9g2nn__z78z!15!^p%7juru%stlgz#^b_qvy_$-%r@'
 
 DEBUG = True
-ALLOWED_HOSTS = ['#']
+ALLOWED_HOSTS = ['*']
 
 # 3. التطبيقات المثبتة (مدمجة جميعاً هنا)
 INSTALLED_APPS = [
